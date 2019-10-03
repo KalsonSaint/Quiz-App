84a769609f904b8bcc3d41e53526e02d2780bd4b
KalsonSaint <saintkalson@gmail.com>
2019-10-03 17:33:39 +0100

    Updated Quiz App

--- js/game.js
+++ js/game.js
@@ -69,12 +69,12 @@ choices.forEach(choice => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    const classToApply = 'incorrect';
-    if(selectedAnswer === currentQuestion.answer){
-      
-    }
-
-    getNewQuestion();
+    const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
